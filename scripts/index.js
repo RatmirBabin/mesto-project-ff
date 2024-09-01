f569b796418987290649099167870c742d905308
Ratmir Babin <ratmir-babin@ya.ru>
2024-09-01 19:53:49 +0300

    mesto-project

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
 
+let cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
+
+let placesItem = document.querySelector(".places__item");
+let placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+function Cards(card, deleteCrd) {
+  let cardEl = cardTemplate.querySelector(".places__item").cloneNode(true);
+  cardEl.querySelector(".card__image").src = card.link;
+  cardEl.querySelector(".card__title").textContent = card.name;
+
+  let deleteBtn = cardEl.querySelector(".card__delete-button");
+  deleteBtn.addEventListener("click", () => deleteCrd(cardEl));
+  return cardEl;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCrd(card) {
+  card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((el) => {
+  placesList.append(Cards(el, deleteCrd));
+});
