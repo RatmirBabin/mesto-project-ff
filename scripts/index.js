901f467317f8e6e001234ee1ff18a2f33224d309
Ratmir Babin <ratmir-babin@ya.ru>
2024-09-01 23:38:07 +0300

    mesto new

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,20 @@
 // @todo: Темплейт карточки
 
-let cardTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
 
-let placesItem = document.querySelector(".places__item");
-let placesList = document.querySelector(".places__list");
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
-function Cards(card, deleteCrd) {
-  let cardEl = cardTemplate.querySelector(".places__item").cloneNode(true);
+function createCard(card, deleteCrd) {
+  const cardEl = cardTemplate.querySelector(".places__item").cloneNode(true);
   cardEl.querySelector(".card__image").src = card.link;
+  cardEl.querySelector(".card__image").alt = card.name;
   cardEl.querySelector(".card__title").textContent = card.name;
 
-  let deleteBtn = cardEl.querySelector(".card__delete-button");
+  const deleteBtn = cardEl.querySelector(".card__delete-button");
   deleteBtn.addEventListener("click", () => deleteCrd(cardEl));
   return cardEl;
 }
@@ -28,5 +28,5 @@ function deleteCrd(card) {
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((el) => {
-  placesList.append(Cards(el, deleteCrd));
+  placesList.append(createCard(el, deleteCrd));
 });
